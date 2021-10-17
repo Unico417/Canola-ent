010d52364832956baae61f988d1f61e24b74c9ac
Unico417 <82880551+Unico417@users.noreply.github.com>
2021-10-17 11:56:06 +0900

    Initial Commit

---
+++ js/scrollreveal.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal('.effect-float', {
+    viewFactor: 0.35,
+    distance: '200px',
+    origin: 'top',
+    distance: '10px',
+    duration: 1500
+});
+
+ScrollReveal().reveal('.effect-float-delayed', {
+    viewFactor: 0.35,
+    distance: '200px',
+    origin: 'top',
+    distance: '10px',
+    interval: 250,
+    duration: 1500
+});
---
+++ js/slick.js
@@ -0,0 +1,3 @@
+$('.job-slick').slick({
+
+});
