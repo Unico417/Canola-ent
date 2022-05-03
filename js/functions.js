75e19e09fcd2bdd31ab1f7d2fecbc6ca462a59e2
Unico417 <82880551+Unico417@users.noreply.github.com>
2022-05-03 23:20:41 +0900

    Bar page Renewal #2

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+$('.slick-fade').slick({
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    pauseOnHover: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    zIndex: 5
+});
